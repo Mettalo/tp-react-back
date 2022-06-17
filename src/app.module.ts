144314326b8eb7ca9f1ba44d5846c43331301e1c
Corgniard <antoine.corgniard@gmail.com>
2022-06-17 11:44:12 +0200

    Add variable environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
-import { TopicController } from './topic/topic.controller';
 import { TopicModule } from './topic/topic.module';
-import { TopicService } from './topic/topic.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/tp-task'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.fy6yc.mongodb.net/?retryWrites=true&w=majority`,
+    ),
     TaskModule,
-    TopicModule  
+    TopicModule,
   ],
   controllers: [AppController],
   providers: [AppService],
